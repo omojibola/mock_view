ecee7baf0f0f2a0238efbec36ca7d0b1a03a641d
Jibola <omojibolaorija@gmail.com>
2025-09-02 19:09:32 +0100

    fixed landing page button bug, added github sign in

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,7 @@ export default function LandingPage() {
             <div className='mt-10 flex flex-col sm:flex-row gap-4 justify-center fade-in-element'>
               <Button
                 size='lg'
-                className='bg-white text-black hover:bg-gray-100 rounded-full text-sm px-8 py-6'
+                className='bg-white text-black hover:bg-gray-100 rounded-full text-sm px-8 py-6 z-50'
                 onClick={() => router.push('/auth')}
               >
                 Start Free Practice
@@ -136,9 +136,9 @@ export default function LandingPage() {
             </div>
           </div>
         </div>
-        <div className='absolute top-1/2 left-1/4 w-72 h-72 bg-primary/10 rounded-full blur-3xl float-animation'></div>
+        <div className='absolute top-1/2 left-1/4 w-72 h-72 bg-primary/10 rounded-full blur-3xl float-animation pointer-events-none'></div>
         <div
-          className='absolute bottom-1/4 right-1/4 w-96 h-96 bg-secondary/10 rounded-full blur-3xl float-animation'
+          className='absolute bottom-1/4 right-1/4 w-96 h-96 bg-secondary/10 rounded-full blur-3xl float-animation pointer-events-none'
           style={{ animationDelay: '1s' }}
         ></div>
       </section>
