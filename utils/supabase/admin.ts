8e872cb8ca6fd33077caacc9868f8f0a0f2689e4
Jibola <omojibolaorija@gmail.com>
2025-09-05 00:30:39 +0100

    change webhook to us service role

--- utils/supabase/admin.ts
+++ utils/supabase/admin.ts
@@ -4,11 +4,12 @@ import { createClient } from '@supabase/supabase-js';
 export function createAdminClient() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, // Service role key
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
       auth: {
         autoRefreshToken: false,
         persistSession: false,
+        detectSessionInUrl: false,
       },
     }
   );
